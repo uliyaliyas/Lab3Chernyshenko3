895798012ca718f955c409146783afe13d1582d0
5090969 <5090969@LAPTOP-NB62C61V>
2023-10-22 21:39:20 +0200

    Добавьте файлы проекта.

---
+++ Lab3Chernyshenko3/Program.cs
@@ -0,0 +1,15 @@
+﻿try
+{
+    Console.Write("Введите x:");
+    double x = double.Parse(Console.ReadLine());
+    Console.Write("Введите x:");
+    int n = int.Parse(Console.ReadLine());
+    double s = 0;
+    for (int i = 1; i <= n; i++)
+        s += Math.Pow(x, i) * Math.Cos(i * Math.PI / 3) / 1;
+    Console.WriteLine($"S={s:F2}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
